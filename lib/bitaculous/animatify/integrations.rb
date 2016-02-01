f5b243c806e4067e5e58e6d014b91ba40b9bbd11
Maik Kempe <mkempe@bitaculous.com>
2016-02-01 14:45:40 +0100

    [➠] Refactored Sass and Sprockets integration. [➠] Updated RuboCop configuration. [➠] Minor text changes.

--- lib/bitaculous/animatify/integrations.rb
+++ lib/bitaculous/animatify/integrations.rb
@@ -2,10 +2,24 @@ module Bitaculous
   module Animatify
     module Integrations # :nodoc:
       require_relative 'integrations/sass_integration'
-      SassIntegration.setup
+      Bitaculous::Animatify::Integrations::SassIntegration.append_paths [
+        Bitaculous::Animatify.stylesheets_path,
+        Bitaculous::Animatify.vendor_stylesheets_path
+
+      ]
 
       require_relative 'integrations/sprockets_integration'
-      SprocketsIntegration.setup
+
+      Bitaculous::Animatify::Integrations::SprocketsIntegration.append_paths [
+        Bitaculous::Animatify.fonts_path,
+        Bitaculous::Animatify.images_path,
+        Bitaculous::Animatify.javascripts_path,
+        Bitaculous::Animatify.stylesheets_path,
+        Bitaculous::Animatify.vendor_fonts_path,
+        Bitaculous::Animatify.vendor_images_path,
+        Bitaculous::Animatify.vendor_javascripts_path,
+        Bitaculous::Animatify.vendor_stylesheets_path
+      ]
     end
   end
 end
